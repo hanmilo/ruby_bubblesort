77a7b10406b3c83a55b7961e400b50bc4013c929
Dean <dean@Dean-MacBook-Air.local>
2024-09-23 12:24:23 +0800

    WIP code update

--- bubblesort.rb
+++ bubblesort.rb
@@ -7,13 +7,17 @@ def recursive_sort (array_sort)
   index0 = 0
   index1 = 1
   while index1 <= array_sort.length do
-    array_hold = [array_sorted[index0], array_sorted[index1]].minmax { |value1, value2|
+    array_hold[index0] = [array_sorted[index0], array_sorted[index1]].min { |value1, value2|
       value1 <=> value2
     }
+    array_hold[index1] = [array_sorted[index0], array_sorted[index1]].max { |value1, value2|
+      value1 <=> value2
+    }
+    array_sorted.push(array_hold)
     index0 += 1
     index1 += 1
   end
-  return array_hold
+  return array_sorted
 end
 
 def bubble_sort(array_sort)
